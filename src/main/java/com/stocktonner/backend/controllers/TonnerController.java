3cbf18c9fbab271cd3ee0a439f778da085c97009
Jefferson Torres <jefferson.torrreis@gmail.com>
2026-01-31 18:54:35 -0300

    Update_implements_ConfigBasicSecurity_UserService

--- src/main/java/com/stocktonner/backend/controllers/TonnerController.java
+++ src/main/java/com/stocktonner/backend/controllers/TonnerController.java
@@ -5,6 +5,15 @@ import com.stocktonner.backend.dtos.RequestTonnerDTO;
 import com.stocktonner.backend.dtos.TonnerDTO;
 import com.stocktonner.backend.services.TonnerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,3 +49,5 @@ public class TonnerController {
         return tonnerService.insert(dto);
     }
 }
+
+
